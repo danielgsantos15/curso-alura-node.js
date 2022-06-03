60e67bd5ad239af69c03e78f11e214b0b205afe7
danielgsantos15 <danir877@gmail.com>
2022-06-02 22:35:21 -0300

    feat: URL validator

--- cli.js
+++ cli.js
@@ -1,11 +1,14 @@
 import pegaArquivo from './public/index.js'
 import chalk from 'chalk'
+import validaURLs from './http-validacao.js'
 
 const caminho = process.argv
 
 async function processaTexto(caminhoDoArquivo) {
   const resultado = await pegaArquivo(caminhoDoArquivo[2])
-  console.log(chalk.yellow('lista de links'), resultado)
+  if (caminho[3] === 'validar' ) {
+    console.log(chalk.yellow('links validados'), await validaURLs(resultado))
+  } else console.log(chalk.yellow('lista de links'), resultado)
 }
 
 processaTexto(caminho)
