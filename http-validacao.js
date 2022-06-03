68c666940b02f7b37588477a191ad377ed9d81a5
danielgsantos15 <danir877@gmail.com>
2022-06-02 22:33:17 -0300

    feat: URL validator

---
+++ http-validacao.js
@@ -0,0 +1,21 @@
+import fetch from 'node-fetch'
+
+async function checaStatus(arrayURLs) {
+  const arrayStatus = await Promise.all(arrayURLs.map(async url => {
+    const res = await fetch(url)
+    return res.status
+  }))
+  return arrayStatus
+}
+
+function geraArrayDeURLs(arrayLinks) {
+  return arrayLinks[0].map(objetoLink => Object.values(objetoLink).join())
+}
+
+async function validaURLs(arrayLinks) {
+  const links = geraArrayDeURLs(arrayLinks)
+  const statusLinks = await checaStatus(links)
+  return statusLinks
+}
+
+export default validaURLs
