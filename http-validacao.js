d43db910b1e1f8ea7e646ed78c2ef9a79b195710
danielgsantos15 <danir877@gmail.com>
2022-06-06 19:51:56 -0300

    feat: riding the object

--- http-validacao.js
+++ http-validacao.js
@@ -1,21 +1,31 @@
 import fetch from 'node-fetch'
 
 async function checaStatus(arrayURLs) {
-  const arrayStatus = await Promise.all(arrayURLs.map(async url => {
-    const res = await fetch(url)
-    return res.status
+  const arrayStatus = await Promise
+  .all(arrayURLs
+    .map(async url => {
+      const res = await fetch(url)
+      return res.status
   }))
   return arrayStatus
 }
 
 function geraArrayDeURLs(arrayLinks) {
-  return arrayLinks[0].map(objetoLink => Object.values(objetoLink).join())
+  return arrayLinks[0]
+  .map(objetoLink => Object
+    .values(objetoLink).join())
 }
 
 async function validaURLs(arrayLinks) {
   const links = geraArrayDeURLs(arrayLinks)
   const statusLinks = await checaStatus(links)
-  return statusLinks
+  const results = Object.assign(statusLinks)
+
+  const resultados = arrayLinks[0].map((objeto, i) => ({
+    ...objeto,
+    status: statusLinks[i]
+  }))
+  return resultados
 }
 
 export default validaURLs
