a54ed6638a8404f75e9d8621d2bfb6e1d7e1b6e6
danielgsantos15 <danir877@gmail.com>
2022-06-09 16:51:40 -0300

    feat: unit testing

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,17 @@
 import pegaArquivo from '../public/index.js'
 
-test('deve ser uma função', () => {
-  expect(typeof pegaArquivo).toBe('function')
+const arrayResult = [
+  {
+    FileList: 'https://developer.mozilla.org/pt-BR/docs/Web/API/FileList'
+  }
+]
+
+describe('pegaArquivo::', () => {
+  it('deve ser uma função', () => {
+    expect(typeof pegaArquivo).toBe('function')
+  })
+  it('deve retornar array com resultados', async () => {
+    const resultado = await pegaArquivo('./test/arquivos')
+    expect(resultado).toEqual(arrayResult)
+  })
 })
